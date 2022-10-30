1ce65d9ca73d5cdd56523a203305275e09bec3c7
Tatayan-san <tany.pligynova.com>
2022-10-30 21:04:35 +0300

    added HTML table

--- lib/Generation_references/HTML_table.rb
+++ lib/Generation_references/HTML_table.rb
@@ -1,75 +1,18 @@
 require_relative 'parsing_anime_facts'
+require 'erb'
 
+def create_table_anime_facts(path)
+  erb_str = File.read(File.dirname(__FILE__) + '/anime_facts_page.html.erb')
 
-def create_table_anime_facts(anime_name, fact_id)
-  file_html = File.new("lib/Generation_references/HTML_table.html", "w+")
   anime_name = Get_Anime()
-  file_html.puts "<style type='text/css'>
-                  .table {
-	                    width: 100%;
-	                    border: none;
-	                    margin-bottom: 20px;
-                  }
-                  .table thead th {
-	                    font-weight: bold;
-	                    text-align: left;
-	                    border: none;
-	                    padding: 10px 15px;
-	                    background: #d8d8d8;
-	                    font-size: 14px;
-                  }
-                  .table thead tr th:first-child {
-	                    border-radius: 8px 0 0 8px;
-                  }
-                  .table thead tr th:last-child {
-	                    border-radius: 0 8px 8px 0;
-                  }
-                  .table tbody td {
-	                    text-align: left;
-	                    border: none;
-	                    padding: 10px 15px;
-	                    font-size: 14px;
-	                    vertical-align: top;
-                  }
-                  .table tbody tr:nth-child(even){
-	                    background: #f3f3f3;
-                  }
-                  .table tbody tr td:first-child {
-	                    border-radius: 8px 0 0 8px;
-                  }
-                  .table tbody tr td:last-child {
-	                    border-radius: 0 8px 8px 0;
-                  }
-                  </style>
-                  <table class='table'>
-                        <thead>
-                            #{anime_name[:anime_name]}
-                            <tr>
-                                <th>Anime</th>
-                            </tr>
-                        </thead>
-                  <tbody>"
+  anime_facts_information = Information_Of_Anime_Facts(Get_Anime())
 
-  anime_facts_information = Information_Of_Anime_Facts(anime_name, fact_id)
-  anime_facts_information.each do |elem|
-    file_html.puts "<tr>"
-    file_html.puts "<th>"
-    if elem[:anime_name].nil?
-      file_html.puts "<span style = 'font-size:20pt'> - </span>"
-    else
-      file_html.puts elem[:anime_name]
-    end
-    file_html.puts "</th>"
-    file_html.puts "<th>"
-    if elem[:fact].nil?
-      file_html.puts "<span style = 'font-size:20pt'> - </span>"
-    else
-      file_html.puts elem[:fact]
-    end
-    file_html.puts "</th>"
-    file_html.puts "<th>"
+  @anime_name = anime_name[:name]
+  @anime_facts_information = anime_facts_information
+  renderer = ERB.new(erb_str)
+  result = renderer.result(binding)
+
+  File.open(path + "/table_game.html", 'w') do |f|
+    f.write(result)
   end
-  file_html.puts "</tbody>
-                  </table>"
-  file_html.close
 end
