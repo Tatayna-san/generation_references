84f0eb68f09ddc2918448fadbdb2f4bb59113139
Tatayan-san <tany.pligynova.com>
2022-10-30 21:05:10 +0300

    added HTML table implementation

--- lib/Generation_references/HTML_main.rb
+++ lib/Generation_references/HTML_main.rb
@@ -1,33 +1,24 @@
-def create_main_table()
-  file_html = File.new('lib/Generation_references/HTML_table.html', 'w+')
-  file_html.puts "<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Заголовок документа</title>
-    <style>
-      div1
-      {
-        width: 100%; /* Размеры */
-        outline: '0';
-        border: '0'; /* Белая рамка */
-      }
-      div2
-      {
-        border-radius: 30%;
-      }
-    </style>
-  </head>
-  <body>
-  <div1>
-  <p border='0'>
-    <div2>
-    <a href='HTML_table.html'><img src='../../images/logo.png' width=45% border='0' alt=''></a>
-    </div2>
-  </p>
-  </div1>
-</body>
-</html>"
-  file_html.close
+require_relative 'html_anime_facts_table'
+require 'erb'
+
+
+module Generation_references
+  module Html_main
+    class Html_main_page
+      def self.create_main(path)
+        create_table_anime_facts(path)
+
+        erb_str = File.read(File.dirname(__FILE__) + '/main_page.html.erb')
+
+        @path = path
+        renderer = ERB.new(erb_str)
+        result = renderer.result(binding)
+
+        File.open(path + "/table_main.html", 'w') do |f|
+          f.write(result)
+        end
+      end
+    end
+  end
 end
 
-#create_main_table()
