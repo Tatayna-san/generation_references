4a1974e518a9947106c9e87173d8e9174d4f7cdd
Tatayan-san <tany.pligynova.com>
2022-10-30 21:19:18 +0300

    added HTML table

--- lib/Generation_references/HTML_table.rb
+++ lib/Generation_references/HTML_table.rb
@@ -5,9 +5,9 @@ def create_table_anime_facts(path)
   erb_str = File.read(File.dirname(__FILE__) + '/anime_facts_page.html.erb')
 
   anime_name = Get_Anime()
-  anime_facts_information = Information_Of_Anime_Facts(Get_Anime())
+  anime_facts_information = Information_Of_Anime_Facts(anime_name)
 
-  @anime_name = anime_name[:name]
+  @anime_name = anime_name
   @anime_facts_information = anime_facts_information
   renderer = ERB.new(erb_str)
   result = renderer.result(binding)
