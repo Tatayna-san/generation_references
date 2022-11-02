d29c0b079c2d99eafadf4306825e0733c2ea30f4
Tatayan-san <tany.pligynova.com>
2022-11-02 14:29:04 +0300

    new commit

---
+++ lib/Generation_references/Anime_Facts/parsing_anime_facts.rb
@@ -0,0 +1,45 @@
+require 'open-uri'
+require 'nokogiri'
+
+def Get_Anime()
+  res = []
+  begin
+    url = Nokogiri::HTML5(URI.open('https://anime-facts-rest-api.herokuapp.com/api/v1')).to_s
+  rescue
+    return res
+  end
+  arr = url.split(',')
+  arr.each do |a|
+    name = a.match(/"anime_name":"(?<name>.*)"/)
+    if !name.nil?
+      res.push( {name: name[:name]})
+    end
+  end
+  res
+end
+
+def Information_Of_Anime_Facts(anime_names)
+  res = []
+  facts = []
+  anime_names.each do |name|
+    begin
+      url = Nokogiri::HTML5(URI.open('https://anime-facts-rest-api.herokuapp.com/api/v1/' + name[:name])).to_s
+    rescue
+      next
+    end
+    anime_name = name[:name]
+    anime_fact = url.split(',')
+    anime_fact.each do |a|
+      fact = a.match(/"fact":"(?<fact>.*)"/)
+      if !fact.nil?
+        res.push( {name: anime_name, fact: fact[:fact]})
+      end
+
+    end
+  end
+  return res
+end
+
+#Anime_Facts = Information_Of_Anime_Facts(Get_Anime())
+#puts Anime_Facts
+#puts Anime_Facts.size
