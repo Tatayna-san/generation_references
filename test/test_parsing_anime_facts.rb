ba7605c12f20628c0270b14b45f92efdfbf9715a
Tatayan-san <tany.pligynova.com>
2022-11-03 21:28:13 +0300

    new commit

--- test/test_parsing_anime_facts.rb
+++ test/test_parsing_anime_facts.rb
@@ -2,14 +2,13 @@ require_relative '../test/test_helper'
 require_relative '../lib/Generation_references/Anime_Facts/parsing_anime_facts.rb'
 
 class Parsing_Anime_Facts < Minitest::Test
-
   def test_of_anime_name
-    anime_facts_information = Anime_Facts.new('name:"bleach",fact:"Ichigo s English Voice Actor Was A Power Ranger')
-    assert_equal(anime_facts_information.name, "bleach")
+    anime_facts = Anime_Facts.new('name:"bleach",fact:"Ichigo s English Voice Actor Was A Power Ranger')
+    assert_equal(anime_facts.name, "bleach")
   end
 
   def test_of_anime_fact
-    anime_facts_information = Anime_Fact.new('name:"bleach",fact:"Ichigo s English Voice Actor Was A Power Ranger')
-    assert_equal(anime_facts_information.fact, "Ichigo s English Voice Actor Was A Power Ranger")
+    anime_facts = Anime_Fact.new('name:"bleach",fact:"Ichigo s English Voice Actor Was A Power Ranger')
+    assert_equal(anime_facts.fact, "Ichigo s English Voice Actor Was A Power Ranger")
   end
 end
